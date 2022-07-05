8dcb162723a11928107cb3fbda71895377a5dacc
hamedhajiloo <hamedhajiloo@hotmail.com>
2022-07-05 21:13:25 +0430

    analyze approches

--- TestSQLUpdateSpeed/Program.cs
+++ TestSQLUpdateSpeed/Program.cs
@@ -3,14 +3,24 @@ using System.Diagnostics;
 using TestSQLUpdateSpeed;
 using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
 
+
+
+var sw1 = Stopwatch.StartNew();
+await Update1();
+sw1?.Stop();
+
+Console.WriteLine($"update 1: {sw1.ElapsedMilliseconds}");
+
+
+var sw2 = Stopwatch.StartNew();
+await Update2();
+sw2?.Stop();
+
+Console.WriteLine($"update 2: {sw2.ElapsedMilliseconds}");
+
 await Show();
-//var sw1 = Stopwatch.StartNew();
-//await Updatea1();
-//sw1?.Stop();
 
-//Console.WriteLine($"update 1: {sw1.ElapsedMilliseconds / 1000}");
-
-//Console.ReadKey();
+Console.ReadKey();
 
 
 async Task Show()
@@ -20,18 +30,25 @@ async Task Show()
     Console.WriteLine(test.Text);
 }
 
-async Task Updatea1()
+async Task Update1()
 {
     await using var db = new MyDbContext();
     var test = await db.Tests.Where(c => c.Id == 1).FirstOrDefaultAsync();
-    test.Text += $"{DateTimeOffset.Now.ToString("ddHHmmss")}";
-    await db.SaveChangesAsync();
+    for (int i = 0; i < 10000; i++)
+    {
+        test.Text += $"update1 {DateTimeOffset.Now.ToString("ddHHmmss")}";
+        await db.SaveChangesAsync(); 
+    }
 }
 
 
-async Task Updatea2()
+async Task Update2()
 {
-   
+    await using var db = new MyDbContext();
+    for (int i = 0; i < 10000; i++)
+    {
+        db.Database.ExecuteSqlRaw("exec AppendText @text={0}", $"update2 {DateTimeOffset.Now.ToString("ddHHmmss")},"); 
+    }
 }
 
 
